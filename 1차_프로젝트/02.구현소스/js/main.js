35033660bb3bfcdfc58933cce59bb590d39579e5
minji73 <kimminji7373@gmail.com>
2024-12-06 17:50:39 +0900

    1

--- 1차_프로젝트/02.구현소스/js/main.js
+++ 1차_프로젝트/02.구현소스/js/main.js
@@ -1,22 +1,17 @@
 // exhibition swiper js
-const swiper = new Swiper('.swiper', {
+$(() => {
+  const swiper = new Swiper(".ex-slide", {
+    // 스와이퍼 여러개 쓰는 경우를 대비해서 구분하는 클래스 하나 주기!
     // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
+    direction: "horizontal", // horizontal > 가로방향드래그, vertical > 세로방향드래그
+    loop: true, // 슬라이드가 끝난 후 처음으로 돌아가면 true, 아니면 false
+    slidesPerView: 3, // 한번에 보여 줄 슬라이드 수
+    spaceBetween: 25, // 슬라이드간의 간격
+
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    }, // 이전, 다음 버튼 원하면 추가
   });
+});
