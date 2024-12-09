947f2ee22d64b1ff1e3cdcd1a631fe4ded96a597
minji73 <kimminji7373@gmail.com>
2024-12-09 17:50:57 +0900

    1

--- 1차_프로젝트/02.구현소스/js/main.js
+++ 1차_프로젝트/02.구현소스/js/main.js
@@ -4,7 +4,7 @@ $(() => {
     // 스와이퍼 여러개 쓰는 경우를 대비해서 구분하는 클래스 하나 주기!
     // Optional parameters
     direction: "horizontal", // horizontal > 가로방향드래그, vertical > 세로방향드래그
-    loop: true, // 슬라이드가 끝난 후 처음으로 돌아가면 true, 아니면 false
+    // loop: true, // 슬라이드가 끝난 후 처음으로 돌아가면 true, 아니면 false
     slidesPerView: 3, // 한번에 보여 줄 슬라이드 수
     spaceBetween: 25, // 슬라이드간의 간격
 
