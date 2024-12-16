8de5068f0f9f3d15c033fb22c04365d2f942857f
kimeming <kimeming030@gmail.com>
2024-12-16 16:53:54 +0900

    아트갤러리 데이터

---
+++ 1차_프로젝트/02.구현소스/js/data.js
@@ -0,0 +1,54 @@
+const artData = [
+    {
+        idx: 1,
+        title: "제목 1",
+        desc: "설명 1"
+    },
+    {
+        idx: 2,
+        title: "제목 2",
+        desc: "설명 2"
+    },
+    {
+        idx: 3,
+        title: "제목 3",
+        desc: "설명 3"
+    },
+    {
+        idx: 4,
+        title: "제목 4",
+        desc: "설명 4"
+    },
+    {
+        idx: 5,
+        title: "제목 5",
+        desc: "설명 5"
+    },
+    {
+        idx: 6,
+        title: "제목 6",
+        desc: "설명 6"
+    },
+    {
+        idx: 7,
+        title: "제목 7",
+        desc: "설명 7"
+    },
+    {
+        idx: 8,
+        title: "제목 8",
+        desc: "설명 8"
+    },
+    {
+        idx: 9,
+        title: "제목 9",
+        desc: "설명 9"
+    },
+    {
+        idx: 10,
+        title: "제목 10",
+        desc: "설명 10"
+    },
+];
+
+export { artData };
