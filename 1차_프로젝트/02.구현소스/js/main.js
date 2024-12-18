64da537de8e9f379070ec77db09dba3f2d1faa73
kimeming <kimeming030@gmail.com>
2024-12-18 16:01:48 +0900

    vote js 애니메이션 추가

--- 1차_프로젝트/02.구현소스/js/main.js
+++ 1차_프로젝트/02.구현소스/js/main.js
@@ -36,4 +36,27 @@ $(() => {
     $("body").removeClass("apricot");
   });
 
+  // vote scroll ani js
+  const io = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      // entry의 target으로 DOM에 접근합니다.
+      const $target = entry.target;
+      // 화면에 노출 상태에 따라 해당 엘리먼트의 class를 컨트롤 합니다.
+      if (entry.isIntersecting) {
+        setTimeout(function(){
+          $target.classList.add("on");
+        }, 1000);
+      } else {
+        $target.classList.remove("on");
+      }
+    });
+  });
+  // 옵저버할 대상 DOM을 선택하여 관찰을 시작합니다.
+  const $item = document.querySelector(".vcard .card-list");
+  io.observe($item);
+  // 특정 요소만 옵저버를 해제합니다.
+  // io.unobserve(targetElement);
+  // 옵저버 전체를 해제합니다.
+  // io.disconnect();
+
 });
